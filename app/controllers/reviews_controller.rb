dd41e42fd11b59f1189491b0db0451c58ff603c0
Student <example@example.com>
2019-08-09 14:17:11 -0700

    continue building songs views, controllers

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,28 @@
+class ReviewsController < ApplicationController
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+    render :new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :show
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:author, :content_body, :rating)
+    end
+end
