c629056ca45c6ab1c7c1248c4ea3182381a22b88
Student <example@example.com>
2019-08-23 10:35:06 -0700

    make progress towards thorough testing of application

---
+++ spec/models/review_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Review do
+
+  it { should belong_to(:product) }
+  it { should validate_presence_of(:author)}
+  it { should validate_length_of(:content_body).is_at_least(50).is_at_most(250)}
+  it { should validate_inclusion_of(:rating).in_range(1..5) }
+end
--- spec/review_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-describe Review do
-  it
-  it { should belong_to(:product) }
-end
