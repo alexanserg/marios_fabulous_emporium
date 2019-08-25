be9d807c12967c26ff1c7929b5ccab944f10463f
Student <example@example.com>
2019-08-25 14:08:54 -0700

    continue to make testing more comprehensive

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe Product do
   it { should have_many(:reviews) }
+  it { should have_db_column(:name) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:cost) }
   it { should validate_presence_of(:country_of_origin) }
